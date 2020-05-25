1826fc809f91fbad847b10fa22c93566a91b2597
Dasha <ddase4kaa@mail.ru>
2020-05-25 18:11:23 +0400

    Simple test (insure that maven is working)

---
+++ src/test/java/lab5/Main.java
@@ -0,0 +1,14 @@
+package lab5;
+import org.testng.annotations.*;
+class Main
+{
+    @Test
+    public void simpleTest()
+    {
+
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
